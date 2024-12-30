f1555889f936a2d24ddd2e092737df64671c2c82
sflins <sflins@gmail.com>
2024-12-30 23:06:41 +0000

    First commit

---
+++ logai-python/chatbot/chat.sh
@@ -0,0 +1,7 @@
+clear
+export MONGODB_URL='mongodb+srv://loganalysis:Dn750102@loganalysismongodb.qvevx.mongodb.net'
+export MONGODB_DBNAME='logs_database'
+
+python3 install -r requiriments.txt
+
+python3 app.py
