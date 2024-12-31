7531ae98b09be9cf8fda84baf73a83e35b0a7e66
sflins <sflins@gmail.com>
2024-12-31 01:46:27 +0000

    Ollama setup

--- logai-python/chatbot/chat.sh
+++ logai-python/chatbot/chat.sh
@@ -2,6 +2,15 @@ clear
 export MONGODB_URL='mongodb+srv://loganalysis:Dn750102@loganalysismongodb.qvevx.mongodb.net'
 export MONGODB_DBNAME='logs_database'
 
-python3 install -r requiriments.txt
+pip install -r requirements.txt
+
+sudo snap install ollama
+#pip install ollama
+
+ollama pull phi
+ollama pull llama2
+ollama pull codellama
+
+sudo snap restart ollama.listener
 
 python3 app.py
--- logai-python/load/load.sh
+++ logai-python/load/load.sh
@@ -2,16 +2,15 @@ clear
 export MONGODB_URL='mongodb+srv://loganalysis:Dn750102@loganalysismongodb.qvevx.mongodb.net'
 export MONGODB_DBNAME='logs_database'
 
-export GITREPO_TOKEN='
+export GITREPO_TOKEN=''
 export GITREPO_OWNER='sflins'
-export GITREPO_NAME='loganalysis'
+export GITREPO_NAME='ialoganalysis'
 export GITREPO_BRANCH='main'
 
-export LOGS_DIR='/home/ubuntu/projects/loganalysis/logai-python/store/logs'
-export SPRINGBOOT_GUIDE_PDF='/home/ubuntu/projects/loganalysis/logai-python/store/docs/spring-boot-reference.pdf'
+export LOGS_DIR='/home/ubuntu/projects/ialoganalysis/logai-python/store/logs'
+export SPRINGBOOT_GUIDE_PDF='/home/ubuntu/projects/ialoganalysis/logai-python/store/docs/spring-boot-reference.pdf'
 
-
-python3 install -r requiriments.txt
+pip install -r requirements.txt
 
 python3 data-pipeline.py
 
